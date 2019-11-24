f674afe054cb62e1f6e1c1826a2a5cfecfc59770
AbhimanyuBassi <abhimanyu.bassi@gmail.com>
2019-11-24 11:41:46 +0800

    Code Refactoring

--- src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -1,16 +1,11 @@
 package com.example.demo.model.requests;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class CreateUserRequest {
 
-	@JsonProperty
 	private String username;
 
-	@JsonProperty
 	private String password;
 
-	@JsonProperty
 	private String confirmPassword;
 
 	public String getUsername() {
